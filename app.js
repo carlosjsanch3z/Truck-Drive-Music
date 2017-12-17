6c43644abbd60dd4ba887f1267fec10bc995d847
Carlos Jesús Sánchez Ortega <carlosjsanchezortega@gmail.com>
2017-12-17 03:30:14 +0100

    Probando controlador de usuario

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+// Routes
+var user_routes = require('./routes/user');
+
+
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-app.get('/pruebas', function(req, res){
-  res.status(200).send({message: 'Bienvenido a la aplicación escrita en NodeJS'});
-});
+// Base
+
+app.use('/api', user_routes);
+
 
 module.exports = app;
